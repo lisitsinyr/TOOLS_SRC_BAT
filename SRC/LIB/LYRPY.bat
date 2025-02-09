7497869ea0d723435d70fbbd555cfbdd528e3736
lisitsinyr <lisitsinyr@gmail.com>
2025-02-09 16:32:51 +0400

    20250209163249

---
+++ SRC/LIB/LYRPY.bat
@@ -0,0 +1,60 @@
+@echo off
+rem -------------------------------------------------------------------
+rem LYRPY.bat
+rem -------------------------------------------------------------------
+chcp 1251>NUL
+
+:begin
+    rem Выход из сценария. Дальше - только функции.
+    exit /b 0
+:end
+
+rem =================================================
+rem ФУНКЦИИ
+rem =================================================
+
+rem -----------------------------------------------
+rem procedure PY_ENV_START ()
+rem -----------------------------------------------
+:PY_ENV_START
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_SetROOT
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    rem echo PY_ENVDIR:!PY_ENVDIR!
+    rem echo PY_ENVNAME:!PY_ENVNAME!
+    call !PY_ENVDIR!\!PY_ENVNAME!\Scripts\activate.bat
+    rem call D:\PROJECTS_LYR\CHECK_LIST\DESKTOP\Python\VENV\P312\Scripts\activate.bat
+
+    rem echo VIRTUAL_ENV_PROMPT:!VIRTUAL_ENV_PROMPT!
+    rem echo PROMPT:!PROMPT!
+    rem echo PYTHONHOME:!PYTHONHOME!
+    rem echo PATH:!PATH!
+
+    exit /b 0
+rem endfunction
+
+rem -----------------------------------------------
+rem procedure PY_ENV_STOP ()
+rem -----------------------------------------------
+:PY_ENV_STOP
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_SetROOT
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    call !PY_ENVDIR!\!PY_ENVNAME!\Scripts\deactivate.bat
+    rem call D:\PROJECTS_LYR\CHECK_LIST\DESKTOP\Python\VENV\P312\Scripts\deactivate.bat
+
+    rem echo VIRTUAL_ENV_PROMPT:!VIRTUAL_ENV_PROMPT!
+    rem echo PROMPT:!PROMPT!
+    rem echo PYTHONHOME:!PYTHONHOME!
+    rem echo PATH:!PATH!
+
+    exit /b 0
+rem endfunction
