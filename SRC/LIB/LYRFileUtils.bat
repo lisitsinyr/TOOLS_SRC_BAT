5b2cc01a0bd9509719338585c0cdfe784a43bb5d
lisitsinyr <lisitsinyr@gmail.com>
2025-03-03 11:25:06 +0400

    20250303112505

--- SRC/LIB/LYRFileUtils.bat
+++ SRC/LIB/LYRFileUtils.bat
@@ -369,6 +369,7 @@ rem beginfunction
     for %AARG% %%f in (!AMASK!) do (
         rem echo %%~nf%%~xf
         copy "%%f" !ADIR_TO!\        >  NUL
+        call :CheckErrorlevel !FUNCNAME! !errorlevel! 1
         echo File %%f copied ...     >> %LOG_FULLFILENAME%
     )
 
