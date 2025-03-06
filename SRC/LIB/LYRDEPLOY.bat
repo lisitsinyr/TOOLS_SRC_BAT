0ff9538fa314dc88609ec6b6d0774c8112476fa9
lisitsinyr <lisitsinyr@gmail.com>
2025-03-06 20:44:39 +0400

    20250306204438

--- SRC/LIB/LYRDEPLOY.bat
+++ SRC/LIB/LYRDEPLOY.bat
@@ -50,6 +50,7 @@ rem beginfunction
 
     set ADirectory=%~1
     rem echo REPO_WORK:!ADirectory!
+
     if not exist !ADirectory!\ (
         echo ERROR: Каталог !ADirectory! не существует ...
         exit /b 1
